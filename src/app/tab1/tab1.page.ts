154196877738bf966f27ec4e6834326fb429f4da
LAPTOP-E5M1J98K\maili <mailinhh2401@gmail.com>
2021-06-07 17:42:11 +0200

    photo machen, speichern und löschen

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectorRef } from '@angular/core';
+import { Photo, PhotoService } from '../services/photo.service';
+
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +8,47 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  start = new Date();
+  end = new Date();
+  constructor(public photoService: PhotoService
+  ) {
+    this.end.setMinutes(this.end.getMinutes() + 7200);
+    this.setUpProgressBar("#pb", this.start.getTime(), this.end.getTime(), 100);
+  }
+  setUpProgressBar(selector, startTime, endTime, update) {
 
-  constructor() {}
+    var timer
+    var elem = document.querySelector(selector)
+    console.log(elem);
+    var max = endTime - startTime
+    elem.max = max
 
+    var setValue = function () {
+      var currentTime = new Date().getTime()
+      var ellasped = currentTime - startTime
+      if (ellasped >= max) {
+        ellasped = max
+        window.clearTimeout(timer)
+      }
+      elem.value = ellasped
+      var prec = ellasped / max * 100
+      elem.setAttribute("data-label", prec.toFixed(2) + '%')
+    }
+
+    setValue()
+    timer = window.setInterval(setValue, update)
+    return
+
+  }
+
+  addPhotoToGallery() {
+    this.photoService.addNewToGallery();
+  }
+  async ngOnInit() {
+    await this.photoService.loadSaved();
+  }
+
+  deletePictureFromGallery(photo, position) {
+    this.photoService.deletePicture(photo, position);
+  }
 }
